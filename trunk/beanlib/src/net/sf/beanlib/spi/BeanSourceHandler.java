abc4146b1af8e586c35e43a435b3328482b6274e
hanson_char <hanson_char@e16e8604-3a10-0410-aa48-ec897cfb8f9f>
2008-07-11 09:12:01 +0000

    enrich BeanSourceHandler SPI

--- trunk/beanlib/src/net/sf/beanlib/spi/BeanSourceHandler.java
+++ trunk/beanlib/src/net/sf/beanlib/spi/BeanSourceHandler.java
@@ -15,11 +15,13 @@
  */
 package net.sf.beanlib.spi;
 
+import java.lang.reflect.Method;
+
 /**
  * Used to do something to the "from" JavaBean.
  *  
  * @author Joe D. Velopar
  */
 public interface BeanSourceHandler {
-	public void handleBeanSource(Object in);
+	public void handleBeanSource(Object fromBean, Method readerMethod, Object propertyValue);
 }
