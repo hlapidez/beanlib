3bab38fc467d52e2c1d4de302c11a63ae2b468e5
hanson_char <hanson_char@e16e8604-3a10-0410-aa48-ec897cfb8f9f>
2008-11-14 23:25:30 +0000

    javadoc

--- beanlib/src/net/sf/beanlib/spi/BeanPopulatorBaseSpi.java
+++ beanlib/src/net/sf/beanlib/spi/BeanPopulatorBaseSpi.java
@@ -17,19 +17,72 @@ package net.sf.beanlib.spi;
 
 
 /**
- * The base of the Bean Populator SPI.
+ * The base interface of the Bean Populator SPI.
+ * This SPI provides various options to control the propagation behavior of JavaBean properties.
  * 
  * @author Joe D. Velopar
  */
 public interface BeanPopulatorBaseSpi 
 {
+    /**
+     * @param beanPopulatable is similar to {@link DetailedBeanPopulatable} but with a simpler API
+     * that is used to control whether a specific JavaBean property should be propagated
+     * from a source bean to a target bean.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initBeanPopulatable(BeanPopulatable beanPopulatable);
+
+    /**
+     * @param detailedBeanPopulatable is used to control whether a specific JavaBean property
+     * should be propagated from the source bean to the target bean.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initDetailedBeanPopulatable(DetailedBeanPopulatable detailedBeanPopulatable);
+    
+    /**
+     * @param beanSourceHandler can be used to act as a call-back 
+     * (to produce whatever side-effects deemed necessary)
+     * after the property value has been retrieved from the source bean, 
+     * but before being propagated across to the target bean.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initBeanSourceHandler(BeanSourceHandler beanSourceHandler);
+    
+    /**
+     * @param readerMethodFinder can be used to find the property getter methods of a source JavaBean.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initReaderMethodFinder(BeanMethodFinder readerMethodFinder);
+    
+    /**
+     * @param setterMethodCollector can be used to collect the property setter methods of a target JavaBean.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initSetterMethodCollector(BeanMethodCollector setterMethodCollector);
+
+    /**
+     * @param beanPopulationExceptionHandler can be used to handle any exception thrown.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initBeanPopulationExceptionHandler(BeanPopulationExceptionHandler beanPopulationExceptionHandler);
+
+    /**
+     * Used to control whether debug messages should be logged.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initDebug(boolean debug);
     
+    /**
+     * @param baseConfig is used to conveniently group all the other initializable options into a single unit.
+     * 
+     * @return the current object (ie this) for method chaining purposes.
+     */
     public BeanPopulatorBaseSpi initBeanPopulatorBaseConfig(BeanPopulatorBaseConfig baseConfig);
 }
