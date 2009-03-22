02a05eb42d282d0971c1d24001847b37a2911689
hanson_char <hanson_char@e16e8604-3a10-0410-aa48-ec897cfb8f9f>
2009-03-22 18:59:27 +0000

    Thanks to Eric Raymond (ericraymond@gmail.com): * fix ProtectedReaderMethodFinder to NOT return package private methods * optimize performance of ProtectedReaderMethodFinder and PrivateReaderMethodFinder

--- trunk/beanlib/src/net/sf/beanlib/provider/finder/ProtectedReaderMethodFinder.java
+++ trunk/beanlib/src/net/sf/beanlib/provider/finder/ProtectedReaderMethodFinder.java
@@ -21,49 +21,53 @@ import java.lang.reflect.Modifier;
 import net.sf.beanlib.spi.BeanMethodFinder;
 
 /**
- * Supports finding JavaBean reader method, 
- * which is public, protected or package private.
+ * Supports finding JavaBean reader method which is either public or protected.
  *   
  * @author Joe D. Velopar
  */
 public class ProtectedReaderMethodFinder implements BeanMethodFinder {
-	public Method find(final String propertyName, Object bean) {
-		String s= propertyName;
-		
-		if (Character.isLowerCase(propertyName.charAt(0))) {
-			s = propertyName.substring(0, 1).toUpperCase();
-			
-			if (propertyName.length() > 1)
-				s += propertyName.substring(1);
-		}
-		Class<?> beanClass = bean.getClass();
-		
-		while (beanClass != Object.class) {
-			Method m = null;
-			try {
-				// Find the declared member method of the class or interface,
-				// recursively on super classes and interfaces as necessary.
-				m = beanClass.getDeclaredMethod("get" + s);
-				
-				if (found(m))
-					return m;
-			} catch (NoSuchMethodException ignore) {
-			}
-			try {
-				m = beanClass.getDeclaredMethod("is" + s);
-
-				if (found(m))
-					return m;
-			} catch (NoSuchMethodException ignore) {
-			}
-			// climb to the super class and repeat
-			beanClass = beanClass.getSuperclass();
-		}
-		return null;
-	}
-	
-	boolean found(Method m) {
-		final int modifier = m.getModifiers();
-		return !Modifier.isStatic(modifier) && !Modifier.isPrivate(modifier);
-	}
+    public Method find(final String propertyName, Object bean) {
+        String s = propertyName;
+        
+        if (Character.isLowerCase(propertyName.charAt(0))) {
+            s = propertyName.substring(0, 1).toUpperCase();
+            
+            if (propertyName.length() > 1)
+                s += propertyName.substring(1);
+        }
+        Class<?> beanClass = bean.getClass();
+        final int maxLengthOfGetterMethodName = s.length() + 3;
+        
+        while (beanClass != Object.class) {
+            // Find the declared member method of the class or interface,
+            // recursively on super classes and interfaces as necessary.
+            for (Method m: beanClass.getDeclaredMethods()) {
+                final int mod = m.getModifiers();
+                
+                if (qualified(m, mod)) {
+                    final String methodName = m.getName();
+                    
+                    if (methodName.length() == maxLengthOfGetterMethodName) {
+                        if (methodName.endsWith(s) 
+                        &&  methodName.startsWith("get"))
+                            return m;
+                    }
+                    else if (methodName.length() == maxLengthOfGetterMethodName-1) {
+                        if (methodName.endsWith(s) 
+                        &&  methodName.startsWith("is"))
+                            return m;
+                    }
+                }
+            }
+            // climb to the super class and repeat
+            beanClass = beanClass.getSuperclass();
+        }
+        return null;
+    }
+    
+    boolean qualified(Method m, int mod) {
+        return (Modifier.isPublic(mod) || Modifier.isProtected(mod)) 
+            && !Modifier.isStatic(mod)
+            ;
+    }
 }
